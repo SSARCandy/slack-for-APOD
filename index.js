f615f6dc1b0e99d92f1992fde50bbe3882c77a27
SSARCandy <ssarcandy@gmail.com>
2016-06-23 23:28:00 +0800

    some random text

--- index.js
+++ index.js
@@ -3,13 +3,27 @@ var Slack = require('node-slack');
 var slack = new Slack(config.HOOK_URL);
 var request = require('request');
 
+const someText = [
+  'Wow... :star_and_crescent:',
+  'Amazing~:stars::stars::stars:',
+  'Cool! :star:',
+  'Show you some beautful picture. :star_of_david:'
+];
+
+const icons = [
+  ':star:',
+  ':star2:',
+  ':star_and_crescent:',
+  ':star_of_david:',
+  ':stars:'
+]
 request(`${config.APOD_BASE_URL}?api_key=${config.NASA_API_KEY}`, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     body = JSON.parse(body);
     slack.send({
-      icon_emoji: ':star2:',
+      icon_emoji: icons[Math.floor(Math.random() * icons.length)],
       username: 'APOD',
-      text: 'Wow..',
+      text: someText[Math.floor(Math.random() * someText.length)],
       attachments: [
         {
           "fallback": "Astronomy Picture of today~!",
